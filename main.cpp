10fe9f8fc0531b0c06f2408e1450c6dffdf2732b
Kberz21 <2025446serey@aupp.edu.kh>
2026-01-29 11:26:06 +0700

    testing

--- main.cpp
+++ main.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
 using namespace std;
 
+class employee{
+    private:
+        int id;
+        int salary;
+    public:
+        employee(){
+            id = 0;
+            salary = 0;
+        }
+        employee(int empId, int empSalary){
+            id = empId;
+            salary = empSalary;
+        }
+        void displayInfo(){
+            cout << "Employee ID: " << id << ", Salary: " << salary << endl;
+        }
+};
+
 int main() {
-    cout << "Hello, World!" << endl;
-    cout << "Hiiii";
+    int option =0;
+    while(option==0){
+        int id, salary;
+        cout << "Enter Employee ID: ";
+        cin >> id;
+        cout << "Enter Employee Salary: ";
+        cin >> salary;
+
+        employee emp(id, salary);
+        emp.displayInfo();
+
+        cout << "Do you want to add another employee? (0 for Yes / 1 for No): ";
+        cin >> option;
+    }
     return 0;
 }
