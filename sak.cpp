7a209df8240d8221db4ea43aa2af0355ae67b389
BunsakLay <2025192lay@aupp.edu.kh>
2026-02-02 11:00:32 +0700

    new

--- sak.cpp
+++ sak.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 using namespace std;
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 
 int main() {
     cout << "This is oscarrrr." << endl;
+=======
+int main() {
+    cout << "Hello, World!" << endl;
+>>>>>>> Stashed changes
 =======
 int main() {
     cout << "Hello, World!" << endl;
