8fdd1814b22276a72518f77ae3a1a209cf88ddce
Kberz21 <2025446serey@aupp.edu.kh>
2026-02-02 21:02:23 +0700

    idk

--- src/users.cpp
+++ src/users.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <string>
 #include <ctime>
-#include "attendences.hpp"
-#include "users.hpp"
-#include "tasks.hpp"
+#include "../include/users.hpp"
 using namespace std;
 
 int users::userId = 0;
@@ -23,33 +21,34 @@ users::users(int uid, string n, string em, string s,bool im, string hp, bool ift
 void create_new_user(){
     cout << "Creating a new user..." << endl;
     cout << "Enter name: ";
-    string name;
-    getline(cin, name);
-    cout << "Enter email: ";
-    string email;
-    getline(cin, email);
-    cout << "Enter sex:";
-    string s;
-    getline(cin,s);
-    cout << "Is manager (1 for yes, 0 for no): ";
-    bool im;
-    cin >> im;
-    cin.ignore(); // to ignore the newline character after reading boolean
-    cout << "Enter password: ";
-    string hp;
-    getline(cin, hp);
-    cout << "Is fulltime (1 for yes, 0 for no): ";
-    bool ift;
-    cin >> ift;
-    cin.ignore(); // to ignore the newline character after reading boolean
-    cout << "Enter department: ";
-    string dept;
-    getline(cin, dept);
-    cout << "Enter hourly pay: ";
-    int hpw;
-    cin >> hpw;
-    static int userIdCounter = 1; // Simple counter for user IDs
-    users newUser(userIdCounter, name, email, s, im, hp, ift, dept, hpw);
-    userIdCounter++;
-    cout << "User created successfully!" << endl;
+    
+    // string name;
+    // getline(cin, name);
+    // cout << "Enter email: ";
+    // string email;
+    // getline(cin, email);
+    // cout << "Enter sex:";
+    // string s;
+    // getline(cin,s);
+    // cout << "Is manager (1 for yes, 0 for no): ";
+    // bool im;
+    // cin >> im;
+    // cin.ignore(); // to ignore the newline character after reading boolean
+    // cout << "Enter password: ";
+    // string hp;
+    // getline(cin, hp);
+    // cout << "Is fulltime (1 for yes, 0 for no): ";
+    // bool ift;
+    // cin >> ift;
+    // cin.ignore(); // to ignore the newline character after reading boolean
+    // cout << "Enter department: ";
+    // string dept;
+    // getline(cin, dept);
+    // cout << "Enter hourly pay: ";
+    // int hpw;
+    // cin >> hpw;
+    // static int userIdCounter = 1; // Simple counter for user IDs
+    // users newUser(userIdCounter, name, email, s, im, hp, ift, dept, hpw);
+    // userIdCounter++;
+    // cout << "User created successfully!" << endl;
 };
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <ctime>
 #include "../include/users.hpp"
 using namespace std;
 
